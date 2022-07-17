e8923ed947c6d7232e82c395bd1a90dbbcaec316
Sedera777 <S.Randrianarivolagmail.com>
2022-07-17 11:38:14 +0300

    updating files: creating index.ejs

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 'use strict'
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 7000;
+
+app.use('/public', express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('welcome to the JARVIS APP');
+    res.render('index');
 });
-app.listen(7000, () => {
-    console.log('Listening on port 7000')
+
+app.get('/login', (req, res) => {
+    res.send('login');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 });
