8259e7e91b42f9cac1d8dc2f52b41913315125b9
Sedera777 <S.Randrianarivolagmail.com>
2022-07-19 16:57:25 +0300

    updating files...

--- App.js
+++ App.js
@@ -2,38 +2,20 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 7000;
-const db = require('./db/config')
+const db = require('./db/config');
 
 
 app.use('/public', express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+// Define Routes
+app.use('/', require('./routes/routes'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/login', (req, res) => {
-    res.render('login')
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-});
+    console.log(`Listening on port ${port}`);
+})
