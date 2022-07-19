e13ea67954c296bf8237698081ce120226ddb3b5
Sedera777 <S.Randrianarivolagmail.com>
2022-07-19 15:24:17 +0300

    updating files...

--- App.js
+++ App.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 7000;
+const db = require('./db/config')
+
 
 app.use('/public', express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -13,8 +16,9 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.render('login')
-
 });
+
+
 
 
 
